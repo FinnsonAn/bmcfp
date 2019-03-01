d70294f80c10675e5eb7902482f20b372c0040b6
FinnsonAn <finnson@yeah.net>
2019-03-01 10:20:15 +0800

    第五章练习题答案

--- src/main/java/com/ans/bmcfp/chapter05/chapter05.java
+++ src/main/java/com/ans/bmcfp/chapter05/chapter05.java
@@ -1,8 +1,18 @@
+package com.ans.bmcfp.chapter05;
+
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class chapter05 {
 
+    /**
+     *一个整数可以被分解为多个整数的乘积，例如，6 可以分解为 2x3。
+     * 请使用递归编程的方法，为给定的整数 n，找到所有可能的分解（
+     * 1 在解中最多只能出现 1 次）。
+     * 例如，输入 8，输出是可以是 1x8, 8x1, 2x4, 4x2,
+     * 1x2x2x2, 1x2x4, ……
+     *
+     */
     public static void main(String[] args) {
 
         fff(8, new ArrayList<Integer>());
